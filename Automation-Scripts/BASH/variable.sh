921fc8fee9b5c793eb564ad742178de5239460d5
Ankit Sharma <anku.sharma.0720@gmail.com>
2026-02-13 16:58:27 +0000

    New varible larn in BASH

---
+++ Automation-Scripts/BASH/variable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "What is your name?"
+
+read name
+
+echo "Hello, $name! Welcome to the DevOps journey."
+
+echo "What is your age?"
+
+read age
+
+echo "You are $age years old. Great to have you on board!"
+echo 'Thank you...'
+
+# now i understand how to use variables in bash scripting. I can read user input and store it in variables, and then use those variables to display personalized messages. This is a fundamental aspect of scripting that allows for dynamic and interactive scripts.
