e504a1e5f24e1cf2255afa4de063a8c857aef58a
Ankit Sharma <anku.sharma.0720@gmail.com>
2026-02-17 13:37:52 +0000

    Edit

--- Automation-Scripts/BASH/first.sh
+++ Automation-Scripts/BASH/first.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "Hello world!"
+
+
+# #!/bin/bash: This is called a shebang. It tells the system that this script should be run using the Bash shell. 
+# It's important to include this at the top of your script to ensure it runs correctly.
+
+# To Run Bash Script:
+
+# 1. Save the script to a file, for example, first.sh.
+# 2. Make the script executable by running: chmod +x first.sh
+# 3. Run the script using: ./first.sh
--- Automation-Scripts/BASH/variable.sh
+++ Automation-Scripts/BASH/variable.sh
@@ -13,8 +13,8 @@ read age
 echo "You are $age years old. Great to have you on board!"
 echo 'Thank you...'
 
-# now i understand how to use variables in bash scripting. I can read user input and store it in variables, and then use those variables to display personalized messages. This is a fundamental aspect of scripting that allows for dynamic and interactive scripts.
+# Now i understand how to use variables in bash scripting. 
+# I can read user input and store it in variables, and then use those variables to display personalized messages. 
+# This is a fundamental aspect of scripting that allows for dynamic and interactive scripts.
 
-
-# more variable
 
